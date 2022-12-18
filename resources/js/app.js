b36c44ec282de1c61be4ebc3e7967c808495c6e4
Zhaolin Lau <68846168+zhaolinlau@users.noreply.github.com>
2022-12-19 04:28:03 +0800

    setup bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import './bootstrap';
-
+import '../css/app.min.css';
+import "../../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min'
+import '../../node_modules/mdb-ui-kit/js/mdb.min'
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
 window.Alpine = Alpine;
