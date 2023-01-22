b1210bc9485f724de823c1a2ad965e0a222f7992
Meliesa62 <cb20035@student.ump.edu.my>
2023-01-22 16:47:57 +0800

    simple seeder

---
+++ database/seeders/ApplicationSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Application;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ApplicationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $applications = [
+			[	'level' => 'Undergraduate',
+				'nationality' => 'Non-Malaysian',
+				'campus' => 'Penang',
+				'programme1' => 'Diploma In Finnace',
+				'programme2' => 'Diploma in Accounting',
+				'programme3' => 'Diploma in Information Technology',
+				'fullname' => 'Alex Wang',
+				'nric' => '1427327481278',
+				'phone' => '9071272341',
+				'email' => 'alexwang@gmail.com',
+                'guardianname' => 'Danial Wang',
+				'guardianphone' => '7327481278',
+				'guardiannric' => '7627372341',
+				'guardianemail' => 'danialwang@gmail.com'
+			],
+			[
+				'level' => 'Postgraduate',
+				'nationality' => 'Malaysian',
+				'campus' => 'Cyberjaya',
+				'programme1' => 'Degree in Computer Science (Hons.)',
+				'programme2' => 'Degree in Financial Engineering (Hons.)',
+				'programme3' => 'Degree in Information Technology (Hons.) Information System',
+				'fullname' => 'Amirol Bin Ahmad',
+				'nric' => '950714011273',
+				'phone' => '0112632723',
+				'email' => 'amirol@gmail.com',
+                'guardianname' => 'Roslinda Binti Ali',
+				'guardianphone' => '700204022782',
+				'guardiannric' => '0137372341',
+				'guardianemail' => 'roslindaali@gmail.com'
+			]
+		];
+
+		foreach ($applications as $key => $application) {
+			Application::create($application);
+		}
+    }
+}
