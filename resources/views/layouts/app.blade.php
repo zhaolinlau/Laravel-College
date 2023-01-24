3c83ee6de08832e4c31e99e5a197c77a4aa68ed2
Zhaolin Lau <zhaolinlau@gmail.com>
2023-01-25 01:19:35 +0800

    add live chat widget

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,8 @@
 	<link rel="dns-prefetch" href="//fonts.gstatic.com">
 	@vite(['resources/js/app.js'])
 	<link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
-	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/jszip-2.5.0/dt-1.13.1/b-2.3.3/b-colvis-2.3.3/b-html5-2.3.3/b-print-2.3.3/datatables.min.css"/>
+	<link rel="stylesheet" type="text/css"
+		href="https://cdn.datatables.net/v/bs5/jszip-2.5.0/dt-1.13.1/b-2.3.3/b-colvis-2.3.3/b-html5-2.3.3/b-print-2.3.3/datatables.min.css" />
 </head>
 
 <body>
@@ -18,6 +19,21 @@
 			@yield('content')
 		</main>
 	</div>
+
+	<script>
+		var Tawk_API = Tawk_API || {},
+			Tawk_LoadStart = new Date();
+		(function() {
+			var s1 = document.createElement("script"),
+				s0 = document.getElementsByTagName("script")[0];
+			s1.async = true;
+			s1.src = 'https://embed.tawk.to/63d00d4dc2f1ac1e202f6119/1gnib7u6d';
+			s1.charset = 'UTF-8';
+			s1.setAttribute('crossorigin', '*');
+			s0.parentNode.insertBefore(s1, s0);
+		})();
+	</script>
+
 </body>
 
 </html>
