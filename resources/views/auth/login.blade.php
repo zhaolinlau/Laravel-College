d050d8aa261cff444ca91499bf1f14df37426b67
Zhaolin Lau <zhaolinlau@gmail.com>
2023-01-25 07:47:00 +0800

    add login validation message to login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,27 +7,31 @@
 @section('content')
 	@include('layouts.navbar')
 	<div class="container py-5">
-		<div class="row py-5">
-			<div class="col-md-6 offset-3 py-5">
+		<div class="row py-5 justify-content-center">
+			<div class="col-8 py-5">
+
 				<div class="card my-5">
 					<div class="card-header">{{ __('Login') }}</div>
 
 					<div class="card-body">
-						<form method="POST" action="{{ route('login') }}">
+						<form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate>
 							@csrf
 
 							<div class="row mb-3">
+								@if (session('error'))
+									<span class="alert alert-danger alert-dismissible fade show" role="alert">
+										<strong>{{ session('error') }}</strong>
+										<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+									</span>
+								@endif
 								<label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
 								<div class="col-md-6">
 									<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"
-										value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-									@error('email')
-										<span class="invalid-feedback" role="alert">
-											<strong>{{ $message }}</strong>
-										</span>
-									@enderror
+										value="{{ old('email') }}" required autocomplete="email" autofocus required>
+									<div class="invalid-feedback">
+										Please enter an email.
+									</div>
 								</div>
 							</div>
 
@@ -36,15 +40,14 @@
 
 								<div class="col-md-6">
 									<input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
-										name="password" required autocomplete="current-password">
-
-									@error('password')
-										<span class="invalid-feedback" role="alert">
-											<strong>{{ $message }}</strong>
-										</span>
-									@enderror
+										name="password" required autocomplete="current-password" required>
+									<div class="invalid-feedback">
+										Please fill out this field.
+									</div>
 								</div>
 							</div>
+
+
 
 							<div class="row mb-3">
 								<div class="col-md-6 offset-md-4">
@@ -80,4 +83,22 @@
 	</div>
 
 	@include('layouts.footer')
+
+	<script>
+		(() => {
+			'use strict'
+
+			const forms = document.querySelectorAll('.needs-validation')
+			Array.from(forms).forEach(form => {
+				form.addEventListener('submit', event => {
+					if (!form.checkValidity()) {
+						event.preventDefault()
+						event.stopPropagation()
+					}
+
+					form.classList.add('was-validated')
+				}, false)
+			})
+		})()
+	</script>
 @endsection
