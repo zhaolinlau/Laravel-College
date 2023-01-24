e70b2a98847fc4488363312fac2fcec4a97917de
Zhaolin Lau <zhaolinlau@gmail.com>
2023-01-25 06:33:15 +0800

    call all the seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,20 +7,24 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+	/**
+	 * Seed the application's database.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		// \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+		// \App\Models\User::factory()->create([
+		//     'name' => 'Test User',
+		//     'email' => 'test@example.com',
+		// ]);
 
-        $this->call(ApplicationSeeder::class);
-    }
+		$this->call(ApplicationSeeder::class);
+		$this->call(CreateUserSeeder::class);
+		$this->call(FAQadmissionSeeder::class);
+		$this->call(FAQprogrammeSeeder::class);
+		$this->call(InfoSeeder::class);
+	}
 }
