711fb74c696a6d17048ac41f3a60a1dde6b228d5
Zhaolin Lau <zhaolinlau@gmail.com>
2023-01-25 07:09:39 +0800

    fix layout

--- resources/views/staff.blade.php
+++ resources/views/staff.blade.php
@@ -9,7 +9,7 @@
 	<div class="container py-5">
 		<div class="row justify-content-center py-5">
 			<div class="col py-5">
-				<div class="row">
+				<div class="row py-5 my-5">
 					<div class="col-4">
 						<div class="card">
 							<div class="card-body">
@@ -18,6 +18,9 @@
 								<a class="btn btn-primary" href="{{route('staff.banner_list')}}">Banner List</a>
 							</div>
 						</div>
+					</div>
+
+					<div class="col-4">
 						<div class="card">
 							<div class="card-body">
 								<h5 class="card-title">Student Application</h5>
@@ -25,11 +28,14 @@
 								<a class="btn btn-primary" href="/index_application">View Application</a>
 							</div>
 						</div>
+					</div>
+
+					<div class="col-4">
 						<div class="card">
 							<div class="card-body">
-								<h5 class="card-title">Carousel Management</h5>
+								<h5 class="card-title">Course list</h5>
 								<p class="card-text">Navigate to list of Course Information.</p>
-								<a class="btn btn-primary" href="{{route('staff.banner_list')}}">Student Application</a>
+								<a class="btn btn-primary" href="">Student Application</a>
 							</div>
 						</div>
 					</div>
